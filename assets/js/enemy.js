3f889700a843124b7c48c3deafcd7c4baa280d95
Nathan-47 <nathanparchment47@gmail.com>
2024-05-30 22:00:48 +0100

    first commit

---
+++ assets/js/enemy.js
@@ -0,0 +1,15 @@
+
+
+
+let enemy;
+
+function Enemy(enemyType, health, motivation, strength, agility, speed) {
+    this.enemyType = enemyType;
+    this.health = health;
+    this.motivation = motivation;
+    this.strength = strength;
+    this.agility = agility;
+    this.speed = speed;
+}
+
+
