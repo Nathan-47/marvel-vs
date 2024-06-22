f72405a37d488136fee28a965763cb8732c2c9a5
Nathan-47 <nathan.parchment47@gmail.com>
2024-06-22 23:04:04 +0100

    updated stats and UI elements

--- components/gamemanager.js
+++ components/gamemanager.js
@@ -1,28 +1,29 @@
 //Object that will control what happens at each stage of the game
 let GameManager = {
-    setGameStart: function(classType) {
-    this.resetPlayer(classType);
+    setGameStart: function(playerType) {
+    this.setPlayer(playerType);
     this.setPreFight();
     },
     
         //This allows for users to choose what fighter they want and be able to see the stats of the fighter.
-        //The class types are seperate via fighters names giving them there unique id.
-    resetPlayer: function(classType) {
-        switch (classType) {
-            case "Adesanya":
-                player = new Player(classType , 190, 100, 70, 90, 90);
+    setPlayer: function(playerType) {
+
+        //Fighter names giving them their unique id.
+        switch (playerType) {
+            case "Hulk":
+                    player = new Player(playerType, 100, 70, 70, 70, 50);
                 break;
             
-                case "Jones":
-                    player = new Player(classType , 200, 75, 95, 80, 90);
+                case "Iron Man":
+                    player = new Player(playerType, 100, 60, 50, 60, 30);
                 break;
             
-                case "McGregor":
-                    player = new Player(classType , 90, 0, 70, 100, 100);
+                case "Wolverine":
+                    player = new Player(playerType, 100, 40, 30, 70, 60);
                 break;
             
-                case "Edwards":
-                    player = new Player(classType , 180, 90, 80, 80, 80);
+                case "Spiderman":
+                    player = new Player(playerType, 100, 40, 50, 30, 50);
                 break;
             
                 default:
@@ -34,59 +35,64 @@ let GameManager = {
         let getInterface = document.querySelector(".interface");
         
         getInterface.innerHTML = '<img src="assets/images/' +
-            classType.toLowerCase() + '.png" class="assets/images"><div><h3>' + player.classType + '</h3><p class="health-player">Health: ' + player.health + '</p><p>Motivation: ' + player.motivation + '</p> <p>Strength: ' + player.strength + '</p> <p>Agility: ' + player.agility + '</p> <p>Speed: ' + player.speed + '</p></div>';
+            playerType.toLowerCase() + '.png" class="assets/images"><div><h3>' + playerType + '</h3><p class="health-player">Health: ' + player.health + '</p></div>';
     },
     
         setPreFight: function () {
-            let getHeader = document.querySelector(".header");
             let getActions = document.querySelector(".actions");
-            let getArena = document.querySelector(".arena");
+
     
-            getHeader.innerHTML = '<p>Task: Find an opponent</p>';
+            TODO:// Place the not Marvel logo here
+            // getHeader.innerHTML = '<p>Task: Find an opponent</p>';
+
+
+            //Search for opponent to battle
             getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.setFight()">Search for Opponent</a>';
-            getArena.style.visibility = "visible";
-    
         },
         
+
+
+
+        // ENEMY BATTLE STAT SETTINGS
+        
         setFight: function () {
-            let getHeader = document.querySelector(".header");
             let getActions = document.querySelector(".actions");
             let getEnemy = document.querySelector(".enemy");
             
-            //Create Opponents
-            let enemy00 = new Enemy("Ferguson", 200, 100, 70, 90, 100);
-            let enemy01 = new Enemy("Nurmagamedov", 200, 90, 100, 70, 80);
-            let enemy02 = new Enemy("Thompson", 180, 70, 70, 80, 80);
-            let enemy03 = new Enemy("Whittaker", 190, 90, 90, 80, 80);
+
+            //Create enemies
+            let enemy00 = new Enemy("Magneto", 100, 20, 40, 60, 30);
+            let enemy01 = new Enemy("Venom", 100, 50, 30, 50, 40);
+            let enemy02 = new Enemy("Abomination", 100, 70, 20, 60, 40);
+            let enemy03 = new Enemy("Sabretooth", 100, 40, 20, 70, 70);
             
-            //Wiil choose a random opponent between 0 - 1
+
+            //Will choose a random opponent between 0 - 4
             let chooseRandomEnemy = Math.floor(Math.random() * Math.floor(4));
     
-            //Console log used to test out the random number generator to see if if outputs the max values needed
-            //console.log(chooseRandomEnemy);
-    
+
+            // Switch between enemy ID for battle
             switch (chooseRandomEnemy) {
                 case 0: enemy = enemy00;
-                    break;
-                
+                break;
+
                 case 1: enemy = enemy01;
-                    break;
+                break;
                 
-                    case 2: enemy = enemy02;
-                    break;
+                case 2: enemy = enemy02;
+                break;
                 
-                    case 3: enemy = enemy03;
-                    break;
-                
+                case 3: enemy = enemy03;
+                break;
             }
-    
-            getHeader.innerHTML = '<p>Task: Choose your move</p>';
+
+
+            // Click to fight
             getActions.innerHTML = '<a href="#" class="btn-prefight" onclick="PlayerMoves.calcAttack()">Fight!</a>';
     
             getEnemy.innerHTML = '<img src="assets/images/enemy/' +
-            enemy.enemyType.toLowerCase() + '.png" class="assets/images/enemy"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p><p>Motivation: ' + enemy.motivation + '</p> <p>Strength: ' + enemy.strength + '</p> <p>Agility: ' + enemy.agility + '</p> <p>Speed: ' + enemy.speed + '</p></div>';
+            enemy.enemyType.toLowerCase() + '.png" class="assets/images/enemy"><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health: ' + enemy.health + '</p></div>';
         }
-    
     }
     
     
